8414eaa02b86d998268f555ef3c87b59a4737cf9
vatanov <atanov.v@gmail.com>
2016-12-13 15:00:53 +0200

    TestBase.cs modified for contact

--- ContactData.cs
+++ ContactData.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebAddressbookTests
 {
-    class ContactData
+    public class ContactData
     {
         // Create fields
         private string firstname = "";
