f7d38cbb02cba99567592d0ca8a5801e5339d7d3
vatanov <atanov.v@gmail.com>
2017-01-24 15:35:04 +0200

    Added assert in ContactRemovalTest

--- tests/ContactCreationTests.cs
+++ tests/ContactCreationTests.cs
@@ -49,20 +49,61 @@ namespace WebAddressbookTests
             oldContacts.Sort();
             newContacts.Sort();
 
-            foreach (ContactData oldContact in oldContacts) 
+            Assert.AreEqual(oldContacts, newContacts);
+        }
+
+        [Test]
+        public void EmptyContactCreationTest()
+        {
+            ContactData contact = new ContactData("");
+            contact.Firstname = "";
+            contact.Middlename = "";
+            contact.Nickname = "";
+            contact.Title = "";
+            contact.Company = "";
+            contact.Address = "";
+            contact.Home = "";
+            contact.Mobile = "";
+            contact.Work = "";
+            contact.Fax = "";
+            contact.Email = "";
+            contact.Email2 = "";
+            contact.Email3 = "";
+            contact.Homepage = "";
+            contact.Bday = "-";
+            contact.Bmonth = "-";
+            contact.Byear = "-";
+            contact.Aday = "-";
+            contact.Amonth = "-";
+            contact.Ayear = "-";
+            contact.Address2 = "";
+            contact.Phone2 = "";
+            contact.Notes = "";
+
+            List<ContactData> oldContacts = app.Contacts.GetContactList();
+
+            app.Contacts.Create(contact);
+
+            Assert.AreEqual(oldContacts.Count + 1, app.Contacts.GetContactCount());
+
+            List<ContactData> newContacts = app.Contacts.GetContactList();
+            oldContacts.Add(contact);
+            oldContacts.Sort();
+            newContacts.Sort();
+
+            foreach (ContactData oldContact in oldContacts)
             {
                 Console.Write(oldContact + "; ");
             }
-           
-            // Console.WriteLine();
-            
+
+            Console.WriteLine();
+
             foreach (ContactData newContact in newContacts)
             {
                 Console.Write(newContact + "; ");
             }
 
             Assert.AreEqual(oldContacts, newContacts);
-
         }
     }
 }
--- tests/ContactRemovalTests.cs
+++ tests/ContactRemovalTests.cs
@@ -13,7 +13,25 @@ namespace WebAddressbookTests
         [Test]
         public void ContactRemovalTest()
         {
+            List<ContactData> oldContacts = app.Contacts.GetContactList();
+            ContactData contactForRemoving = oldContacts[0];
+
             app.Contacts.Remove(1);
+
+            Assert.AreEqual(oldContacts.Count - 1, app.Contacts.GetContactCount());
+
+            List<ContactData> newContacts = app.Contacts.GetContactList();
+
+            oldContacts.RemoveAt(0);
+            oldContacts.Sort();
+            newContacts.Sort();
+            Assert.AreEqual(oldContacts, newContacts);
+
+            // NEED TO CREATE THE SAME ASSERT LIKE FOR GROUPS!!!!
+            //foreach (GroupData group in newGroups)
+            //{
+            //    Assert.AreNotEqual(group.Id, groupForRemoving.Id);
+            //}
         }
     }
 }
